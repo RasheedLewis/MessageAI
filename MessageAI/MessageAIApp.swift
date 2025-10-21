754d459e7d8dc52740ffada7f3cdf3e715a8a1d6
Rasheed Lewis <rasheedlewis1997@gmail.com>
2025-10-21 00:11:00 -0400

    Initial Commit

---
+++ MessageAI/MessageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAIApp.swift
+//  MessageAI
+//
+//  Created by Rasheed Lewis on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MessageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
